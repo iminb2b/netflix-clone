4727597bcd2cdfcb982c7daf7ac79547fd2c23b5
Nhung Nguyen <iminb2b@gmail.com>
2024-02-28 23:37:38 -0500

    set darkmode as default

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -20,7 +20,7 @@ export default function App({ Component, pageProps }: AppProps) {
   const initialContextValue: AppContextType = {
     strings: localeStrings,
     lang: localeInfo === "vi" ? "vi" : "en",
-    darkmode: false,
+    darkmode: true,
   };
 
   useEffect(() => {
